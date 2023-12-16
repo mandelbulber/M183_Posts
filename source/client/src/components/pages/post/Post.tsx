44ba8c38f930a6e8ad73f824b7704bdeb096848d
D1i2m3 <arnold.tim2005@gmail.com>
2023-12-16 23:40:02 +0100

    Add comment form for authenticated users

--- source/client/src/components/pages/post/Post.tsx
+++ source/client/src/components/pages/post/Post.tsx
@@ -4,8 +4,29 @@ import { useParams } from "react-router-dom";
 
 export const Post: FC = () => {
   const [post, setPost] = useState({} as any);
+  const [loggedIn, setLoggedIn] = useState(false);
   const params = useParams();
-  
+
+  const comment = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const userInputs = Object.fromEntries(new FormData(event.currentTarget));
+    userInputs.postId = post.id;
+    userInputs.username = "Horst Konrader";
+    fetch("/api/post/comment", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(
+        userInputs
+      ),
+    }).then((response) => {
+      if (response.status === 200) {
+        window.location.reload();
+      }
+    });
+  }
+
   useEffect(() => {
     fetch(`/api/post/${params.id}`, {
       method: "GET",
@@ -18,6 +39,16 @@ export const Post: FC = () => {
           setPost(data);
         });
       }
+    });
+    fetch("/api/auth/isAuthenticated", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((response) => {
+      response.json().then((data) => {
+        setLoggedIn(data);
+      });
     });
   }, []);
 
@@ -34,15 +65,35 @@ export const Post: FC = () => {
 
       <p>Author: {post.userId}</p>
       <hr />
-      <p>{post.content}</p>
+      <p className={css`
+        margin: 100px 0;
+      `}>{post.content}</p>
+      <hr />
+      <h2>Comments:</h2>
       <ul>
         {post.comments?.map((comment: any) =>
           <li key={comment.id} className={css`
             margin-bottom: 10px;
+            white-space: pre-wrap;
           `}>{comment.userId}:<br />{comment.content}</li>
         )}
       </ul>
 
+      {loggedIn && (
+        <form onSubmit={comment} className={css`
+        margin-top: 50px;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+
+        & > * {
+          margin-bottom: 20px;
+        }
+        `}>
+          <textarea name="content" placeholder="Comment" cols={50} rows={5}></textarea>
+          <input type="submit" value="Comment" />
+        </form>
+      )}
     </div>
   );
 };
